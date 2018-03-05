39437c61ecb33a87ff6a9a34e01bdd7e8b24cf2d
夏爽 <996190264@qq.com>
2018-03-05 10:32:14 +0800

    add session

--- src/singleton/RequestInstance.php
+++ src/singleton/RequestInstance.php
@@ -70,20 +70,6 @@ class RequestInstance extends fromRequest
      */
     public static function __callStatic($method,$args)
     {
-        $request = static::method();
-        switch (count($args)) {
-            case 0:
-                return $request->$method();
-            case 1:
-                return $request->$method($args[0]);
-            case 2:
-                return $request->$method($args[0], $args[1]);
-            case 3:
-                return $request->$method($args[0], $args[1], $args[2]);
-            case 4:
-                return $request->$method($args[0], $args[1], $args[2], $args[3]);
-            default:
-                return call_user_func_array([$request, $method], $args);
-        }
+        return static::method()->$method(...$args);
     }
 }
---
+++ src/tools/Session.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tools\tools;
+
+
+class Session    extends  \Symfony\Component\HttpFoundation\Session\Session
+{
+
+}
